ecda27169e366f6b13e9d677bf83de7a24f5afb3
Michael Marner <michael.marner@unisa.edu.au>
2013-05-10 08:50:48 +0930

    Functional listen now widget.

--- ListenNow.php
+++ ListenNow.php
@@ -17,6 +17,14 @@ class ListenNow extends WP_Widget {
 		echo '/images/ListenNow.png" alt="Listen Now!" class="threed-sidebar-heading"/>';
 	   	echo $after_title;
 
+		echo '<div class="listen_now">';
+		echo '<div class="play_button" >';
+		echo '<a href="http://media.on.net/radio/137.m3u" style="display:block;width:33px;height:33px;">&nbsp</a>';
+		echo '</div>';
+		echo '<a href="http://media.on.net/radio/137.m3u">MP3 Stream</a><br />';
+		echo '<a href="'. get_page_link('361') .'">Other ways to listen</a>';
+		echo '</div>';
+		
 		echo $after_widget;
 	}
 
